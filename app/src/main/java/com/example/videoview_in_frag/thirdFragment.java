dee1266999bbae96fb54f3803577e6602895acfd
ka0306 <kashiimirani03@gmail.com>
2023-08-13 11:59:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/videoview_in_frag/thirdFragment.java
@@ -0,0 +1,30 @@
+package com.example.videoview_in_frag;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+public class thirdFragment extends Fragment {
+
+   VideoView videoView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       View view= inflater.inflate(R.layout.fragment_third, container, false);
+        videoView= view.findViewById(R.id.video3);
+        videoView.setVideoURI(Uri.parse("android.resource://"+view.getContext().getPackageName()+"/"+R.raw.yar));
+        MediaController mediaController = new MediaController(getActivity());
+        videoView.setMediaController(mediaController);
+        videoView.start();
+       return  view;
+    }
+}
